cdd4d50a3df19ee01b9e96e5f07dc84b7f492ae0
Casper Hougaard <casperhgrd@gmail.com>
2015-04-26 21:09:12 +0200

    Added projects. The plugin is still missing

---
+++ Client/GTR24h_ServerManager.Client/ServerManager.Bll/ConnectionProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerManager.Bll
+{
+    class ConnectionProvider
+    {
+
+        public string ReadData()
+        {
+
+            return "Player 1;Ferrari";
+        }
+
+        public void SendCommand(string command)
+        {
+            
+        }
+
+    }
+}
---
+++ Client/GTR24h_ServerManager.Client/ServerManager.Bll/PluginDataProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerManager.Bll
+{
+    class PluginDataProvider
+    {
+        //Start a thread, listening for new data. Raise event, when new data is available
+
+        public event EventHandler<EventArgs> NewDataEvent;
+
+        private Thread ListenerThread;
+
+        public PluginDataProvider()
+        {
+            //
+        }
+
+        public void StartListener()
+        {
+            this.ListenerThread = new Thread(this.ListenerRun);
+
+            this.ListenerThread.Start();
+        }
+
+        private void ListenerRun()
+        {
+            
+        }
+
+    }
+}
